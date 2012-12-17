bef0a488deebe0ac1b88fdd7efbc2f535b6412f6
Syan <develop2k@gmail.com>
2012-12-17 10:41:19 +0100

    updated properties for ExifAux

--- SYPictureMetadata/SYMetadataExifAux.h
+++ SYPictureMetadata/SYMetadataExifAux.h
@@ -10,4 +10,14 @@
 
 @interface SYMetadataExifAux : SYMetadataBase
 
+@property (readonly, nonatomic, getter = getLensInfo)           NSArray*    lensInfo;
+@property (readonly, nonatomic, getter = getLensModel)          NSString*   lensModel;
+@property (readonly, nonatomic, getter = getSerialNumber)       NSString*   serialNumber;
+@property (readonly, nonatomic, getter = getLensID)             NSNumber*   lensID;
+@property (readonly, nonatomic, getter = getLensSerialNumber)   NSString*   lensSerialNumber;
+@property (readonly, nonatomic, getter = getImageNumber)        NSNumber*   imageNumber;
+@property (readonly, nonatomic, getter = getFlashCompensation)  NSObject*   flashCompensation;
+@property (readonly, nonatomic, getter = getOwnerName)          NSString*   ownerName;
+@property (readonly, nonatomic, getter = getFirmware)           NSObject*   firmware;
+
 @end
--- SYPictureMetadata/SYMetadataExifAux.m
+++ SYPictureMetadata/SYMetadataExifAux.m
@@ -3,11 +3,39 @@
 //  SYPictureMetadataExample
 //
 //  Created by rominet on 12/16/12.
-//  Copyright (c) 2012 Syan. All rights reserved.
+//  Copyright (c 2012 Syan. All rights reserved.
 //
 
+#import <ImageIO/ImageIO.h>
 #import "SYMetadataExifAux.h"
 
 @implementation SYMetadataExifAux
+
+-(NSArray *)getLensInfo
+{ return [self arrayForKeyStringRef:kCGImagePropertyExifAuxLensInfo]; }
+
+-(NSString *)getLensModel
+{ return [self stringForKeyStringRef:kCGImagePropertyExifAuxLensModel]; }
+
+-(NSString *)getSerialNumber
+{ return [self stringForKeyStringRef:kCGImagePropertyExifAuxSerialNumber]; }
+
+-(NSNumber *)getLensID
+{ return [self numberForKeyStringRef:kCGImagePropertyExifAuxLensID]; }
+
+-(NSString *)getLensSerialNumber
+{ return [self stringForKeyStringRef:kCGImagePropertyExifAuxLensSerialNumber]; }
+
+-(NSNumber *)getImageNumber
+{ return [self numberForKeyStringRef:kCGImagePropertyExifAuxImageNumber]; }
+
+-(NSObject *)getFlashCompensation
+{ return [self objectForKeyStringRef:kCGImagePropertyExifAuxFlashCompensation]; }
+
+-(NSString *)getOwnerName
+{ return [self stringForKeyStringRef:kCGImagePropertyExifAuxOwnerName]; }
+
+-(NSObject *)getFirmware
+{ return [self objectForKeyStringRef:kCGImagePropertyExifAuxFirmware]; }
 
 @end
